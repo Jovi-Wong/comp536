c1f3e34436301318af927d005a9c977fafa7f190
Jovi Wong <1350386163@qq.com>
2023-01-30 18:25:27 -0600

    /usr

--- homework1/part2/part2.sh
+++ homework1/part2/part2.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-echo "ver1 time"
 i=0
 while (( $i<10 ))
 do
-    mytime="$(time ( ./ver1 ) 2>&1 1>/dev/null)"
+    mytime="$(/usr/bin/time ( ./ver1 ) 2>&1 1>/dev/null)"
     echo $mytime >> ver1_time.txt
     i=`expr $i + 1`
 done
 
-echo "ver2 time"
 i=0
 while (( $i<10 ))
 do
-    mytime="$(time ( ./ver2 ) 2>&1 1>/dev/null)"
+    mytime="$(/usr/bin/time ( ./ver2 ) 2>&1 1>/dev/null)"
     echo $mytime >> ver2_time.txt
     i=`expr $i + 1`
 done
