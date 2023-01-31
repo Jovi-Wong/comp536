6962f2e542193ebff0517f9e0af03c8b9b359d5b
Jovi Wong <1350386163@qq.com>
2023-01-30 19:49:21 -0600

    run 1000 times

--- homework1/part2/part2.sh
+++ homework1/part2/part2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 i=0
-while (( $i<10 ))
+while (( $i<1000 ))
 do
     mytime="$(/usr/bin/time ./ver1 2>&1 1>/dev/null)"
     echo $mytime >> ver1_time.txt
@@ -8,7 +8,7 @@ do
 done
 
 i=0
-while (( $i<10 ))
+while (( $i<1000 ))
 do
     mytime="$(/usr/bin/time ./ver2 2>&1 1>/dev/null)"
     echo $mytime >> ver2_time.txt
