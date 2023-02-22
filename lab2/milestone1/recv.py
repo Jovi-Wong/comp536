58b8a14579c95058b26bbd405fabfe01ad9410a9
Jovi Wong <1350386163@qq.com>
2023-02-21 21:46:05 -0600

    len 2

--- lab2/milestone1/recv.py
+++ lab2/milestone1/recv.py
@@ -39,8 +39,7 @@ def main():
     iface = ifaces[0]
     print("sniffing on %s" % iface)
     sys.stdout.flush()
-    sniff(filter="tcp", iface = iface,
-          prn = lambda x: handle_pkt(x))
+    sniff(filter="tcp", iface = iface, prn = lambda x: handle_pkt(x))
 
 if __name__ == '__main__':
     main()
