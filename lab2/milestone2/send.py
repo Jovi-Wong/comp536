ada895784c7ab35201ca4c8d6da35f44dcfbb5df
Jovi Wong <1350386163@qq.com>
2023-02-22 18:38:47 -0600

    new send

--- lab2/milestone2/send.py
+++ lab2/milestone2/send.py
@@ -17,19 +17,13 @@ def get_if():
     return iface
 
 def main():
+    addr = socket.gethostbyname(sys.argv[1])
+    iface = get_if()
+    for i in range(100):
+        pkt = Ether(src=get_if_hwaddr(iface), dst='ff:ff:ff:ff:ff:ff', type=0x3814) / IP(dst=addr) / TCP(dport=1234, sport=random.randint(49152,65535)) / ("a"*(i+1))
+        sendp(pkt, iface=iface, verbose=False)
 
-    if len(sys.argv) == 3:
-        addr = socket.gethostbyname(sys.argv[1])
-        iface = get_if()
-        print("sending normal packet on interface %s to %s" % (iface, str(addr)))
-        pkt = Ether(src=get_if_hwaddr(iface), dst='ff:ff:ff:ff:ff:ff', type=0x3814) / IP(dst=addr) / TCP(dport=1234, sport=random.randint(49152,65535)) / sys.argv[2]
-        pkt.show2()
-    elif len(sys.argv) == 2:
-        addr = socket.gethostbyname(sys.argv[1])
-        iface = get_if()
-        print("sending query packet on interface %s to %s" % (iface, str(addr)))
-        pkt = Ether(src=get_if_hwaddr(iface), dst='ff:ff:ff:ff:ff:ff', type=0x9723) / IP(dst=addr) / TCP(dport=1234, sport=random.randint(49152,65535)) / PortThrouput()
-        pkt.show2()
+    pkt = Ether(src=get_if_hwaddr(iface), dst='ff:ff:ff:ff:ff:ff', type=0x9723) / IP(dst=addr) / TCP(dport=1234, sport=random.randint(49152,65535)) / PortThrouput()
     sendp(pkt, iface=iface, verbose=False)
 
 if __name__ == '__main__':
